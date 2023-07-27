7a8d89d97aa936e8bb3e3e1c841547f32cbd2823
dasimems <isaacseun63@gmail.com>
2023-07-27 09:56:47 +0100

    update: updated the getting started two screen

--- screens/GettingStartedTwo.jsx
+++ screens/GettingStartedTwo.jsx
@@ -18,7 +18,11 @@ import { lato } from "../fonts";
 import { useNavigation } from "@react-navigation/native";
 import { NavNames, measurements } from "../data/general";
 import { ArrowRight } from "../assets/icons";
-import { OnboardingBackgroundOne, OnboardingOneImage, OnboardingTwoImage } from "../assets/images";
+import {
+  OnboardingBackgroundOne,
+  OnboardingOneImage,
+  OnboardingTwoImage
+} from "../assets/images";
 
 const eclipseMeasurement = measurements.windowWidth * 1.5;
 
@@ -31,8 +35,6 @@ const GettingStartedTwo = () => {
         backgroundColor: whiteColor.default
       }}
     >
-      
-
       <View
         style={{
           flex: 1,
@@ -41,49 +43,55 @@ const GettingStartedTwo = () => {
           overflow: "hidden"
         }}
       >
-
-        <View style={{
-            flex: 4/6,
+        <View
+          style={{
+            flex: 4 / 6,
             position: "relative"
-        }}>
-
-            <View style={{
-                width: eclipseMeasurement,
-                height: eclipseMeasurement,
-                backgroundColor: secondaryColor.light.default,
+          }}
+        >
+          <View
+            style={{
+              width: eclipseMeasurement,
+              height: eclipseMeasurement,
+              backgroundColor: secondaryColor.light.default,
+              borderRadius: eclipseMeasurement,
+              position: "absolute",
+              bottom: -(eclipseMeasurement * 0.43),
+              left: -(eclipseMeasurement / 2),
+              transform: [
+                {
+                  translateX: eclipseMeasurement / 3
+                }
+              ],
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <View
+              style={{
+                width: "80%",
+                height: "80%",
                 borderRadius: eclipseMeasurement,
-                position: "absolute",
-                bottom: -((eclipseMeasurement) * 0.43),
-                left: -((eclipseMeasurement)/2),
-                transform: [{
-                    translateX: ((eclipseMeasurement)/3)
-                }],
-                alignItems: "center",
-                justifyContent: "center"
-
-            }}>
-                <View style={{
-                    width: "80%",
-                    height: "80%",
-                    borderRadius: eclipseMeasurement,
-                    backgroundColor: secondaryColor.lighter.default,
-                    alignItems: "center"
-                }}>
-
-                    <Image source={OnboardingTwoImage} style={{
-                        flex: 1/2,
-                        resizeMode: "contain",
-                        maxWidth: 250,
-                        maxHeight: 250
-                    }} />
-
-                </View>
+                backgroundColor: secondaryColor.lighter.default,
+                alignItems: "center"
+              }}
+            >
+              <Image
+                source={OnboardingTwoImage}
+                style={{
+                  flex: 1 / 2,
+                  resizeMode: "contain",
+                  maxWidth: 250,
+                  maxHeight: 250
+                }}
+              />
             </View>
-
+          </View>
         </View>
-        
-        <View style={{
-            flex: 2/6,
+
+        <View
+          style={{
+            flex: 2 / 6,
             backgroundColor: whiteColor.default,
             borderTopLeftRadius: 50,
             borderTopRightRadius: 50,
@@ -92,53 +100,59 @@ const GettingStartedTwo = () => {
             paddingVertical: 40,
             paddingBottom: 20,
             paddingHorizontal: 20
-        }}>
+          }}
+        >
+          <Text
+            style={{
+              color: blackColor.opacity700,
+              fontFamily: lato.bold.default,
+              fontSize: 22,
+              textAlign: "center",
+              maxWidth: 250
+            }}
+          >
+            Friendly and Verified Drivers
+          </Text>
 
-            <Text style={{
-                color: blackColor.opacity700,
-                fontFamily: lato.bold.default,
-                fontSize: 22,
-                textAlign: "center",
-                maxWidth: 250
+          <Text
+            style={{
+              fontFamily: lato.regular.default,
+              color: blackColor.opacity500,
+              maxWidth: 280,
+              textAlign: "center",
+              fontSize: 15
+            }}
+          >
+            Get to ride with friendly drivers who are also verified
+          </Text>
 
-            }}>Withdraw funds easily from your wallet</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 10
+            }}
+          >
+            <View
+              style={{
+                width: 10,
+                height: 10,
+                backgroundColor: blackColor.opacity200,
+                borderRadius: 100
+              }}
+            />
 
-            <Text style={{
-                fontFamily: lato.regular.default,
-                color: blackColor.opacity500,
-                maxWidth: 280,
-                textAlign: "center",
-                fontSize: 15
-            }}>You can easily withdraw your funds and earning from your wallet</Text>
-
-            <View style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: 10
-            }}>
-
-                <View style={{
-                    width: 10,
-                    height: 10,
-                    backgroundColor: blackColor.opacity200,
-                    borderRadius: 100,
-
-                }}></View>
-
-                <View style={{
-                    width: 10,
-                    height: 10,
-                    backgroundColor: primaryColor.default,
-                    borderRadius: 100,
-                    
-                }}></View>
-
-
-            </View>
+            <View
+              style={{
+                width: 10,
+                height: 10,
+                backgroundColor: primaryColor.default,
+                borderRadius: 100
+              }}
+            />
+          </View>
         </View>
-
-
       </View>
 
       <GettingStartedFooter />
