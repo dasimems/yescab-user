4823f0ab0acb88ca88189c921702710ebc2bf1ea
dasimems <isaacseun63@gmail.com>
2023-07-27 09:55:09 +0100

    update: updated the gettingnstarted one screeb

--- screens/GettingStarted.jsx
+++ screens/GettingStarted.jsx
@@ -1,93 +1,118 @@
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { Button, GettingStartedFooter, Logo } from '../components'
-import { blackColor, primaryColor, secondaryColor, whiteColor } from '../assets/colors'
-import { lato } from '../fonts'
-import { useNavigation } from '@react-navigation/native'
-import { NavNames } from '../data/general'
-import { ArrowRight } from '../assets/icons'
-import { OnboardingBackgroundOne, OnboardingOneImage } from '../assets/images'
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import React from "react";
+import { Button, GettingStartedFooter, Logo } from "../components";
+import {
+  blackColor,
+  primaryColor,
+  secondaryColor,
+  whiteColor
+} from "../assets/colors";
+import { lato } from "../fonts";
+import { useNavigation } from "@react-navigation/native";
+import { NavNames } from "../data/general";
+import { ArrowRight } from "../assets/icons";
+import { OnboardingBackgroundOne, OnboardingOneImage } from "../assets/images";
 
 const GettingStarted = () => {
-    
-    const { navigate } = useNavigation();
+  const { navigate } = useNavigation();
   return (
-    <View style={{
+    <View
+      style={{
         flex: 1,
         backgroundColor: whiteColor.default
-    }}>
+      }}
+    >
+      <Image
+        source={OnboardingBackgroundOne}
+        style={{
+          height: "100%",
+          resizeMode: "contain",
+          width: "100%",
+          position: "absolute"
+        }}
+      />
 
-    <Image source={OnboardingBackgroundOne} style={{
-      height: "100%",
-      resizeMode: "contain",
-      width: '100%',
-      position: "absolute"
-
-    }} />
-
-        <View style={{
-            flex: 1,
-            position: "relative",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            gap: 40,
-            paddingHorizontal: 20
-        }}> 
-
-        <Image source={OnboardingOneImage} style={{
-          flex: 1/1.5,
-          resizeMode: "contain"
-
-        }} />
-
-        <Text style={{
-          color: primaryColor.default,
-          fontFamily: lato.bold.default,
-          maxWidth: 280,
-          textAlign: "center",
-          fontSize: 25
-        }}>Make money when you drive</Text>
-        <Text style={{
-          color: blackColor.opacity500,
-          fontFamily: lato.regular.default,
-          maxWidth: 250,
-          textAlign: "center",
-          fontSize: 15
-        }}>Making money for driving just got easier and better</Text>
-
-        <View style={{
-          flexDirection: "row",
+      <View
+        style={{
+          flex: 1,
+          position: "relative",
+          justifyContent: "flex-end",
           alignItems: "center",
-          justifyContent: "center",
-          gap: 10
-        }}>
+          gap: 40,
+          paddingHorizontal: 20
+        }}
+      >
+        <Image
+          source={OnboardingOneImage}
+          style={{
+            flex: 1 / 1.5,
+            resizeMode: "contain"
+          }}
+        />
 
-          <View style={{
-            width: 10,
-            height: 10,
-            backgroundColor: primaryColor.default,
-            borderRadius: 100,
+        <Text
+          style={{
+            color: primaryColor.default,
+            fontFamily: lato.bold.default,
+            maxWidth: 280,
+            textAlign: "center",
+            fontSize: 25
+          }}
+        >
+          Get a ride in minutes
+        </Text>
+        <Text
+          style={{
+            color: blackColor.opacity500,
+            fontFamily: lato.regular.default,
+            maxWidth: 250,
+            textAlign: "center",
+            fontSize: 15
+          }}
+        >
+          Instantly get a ride to any where you want to go in minutes
+        </Text>
 
-          }}></View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 10
+          }}
+        >
+          <View
+            style={{
+              width: 10,
+              height: 10,
+              backgroundColor: primaryColor.default,
+              borderRadius: 100
+            }}
+          />
 
-          <View style={{
-            width: 10,
-            height: 10,
-            backgroundColor: blackColor.opacity200,
-            borderRadius: 100,
-            
-          }}></View>
-
+          <View
+            style={{
+              width: 10,
+              height: 10,
+              backgroundColor: blackColor.opacity200,
+              borderRadius: 100
+            }}
+          />
         </View>
-            
-        </View>
-
+      </View>
 
       <GettingStartedFooter />
     </View>
-  )
-}
+  );
+};
 
-export default GettingStarted
+export default GettingStarted;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
